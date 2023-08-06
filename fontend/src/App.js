d2e0d6e497e44fb1f85342f93df897fabbe606d5
MhdTarhini <mohamedtarhini95@gmail.com>
2023-08-06 12:33:00 +0300

    install reeact router and leaflet + create routes path

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./Pages/Landing_page";
+import ContactForm from "./component/ContactForm";
+import ContactsMap from "./component/ContactsMap";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/add" element={<ContactForm />} />
+        <Route path="/map" element={<ContactsMap />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ fontend/src/component/ContactsMap.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ContactMap() {
+  return <div></div>;
+}
+
+export default ContactMap;
